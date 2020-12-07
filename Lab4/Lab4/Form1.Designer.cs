eacaff36ef097181b0a63fbc026203350feda523
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2020-12-08 00:29:45 +0300

    Init commit

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lab4DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lab4DataSet = new Lab4.lab4DataSet();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lab4DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lab4DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(682, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            // 
+            // lab4DataSetBindingSource
+            // 
+            this.lab4DataSetBindingSource.DataSource = this.lab4DataSet;
+            this.lab4DataSetBindingSource.Position = 0;
+            // 
+            // lab4DataSet
+            // 
+            this.lab4DataSet.DataSetName = "lab4DataSet";
+            this.lab4DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Location = new System.Drawing.Point(12, 184);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.TabIndex = 1;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(131, 183);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 2;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(259, 182);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 3;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lab4DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lab4DataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource lab4DataSetBindingSource;
+        private lab4DataSet lab4DataSet;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Button BtnDelete;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,83 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        private DataTable dataTable;
+        private MySqlConnection connection;
+        private MySqlCommandBuilder mySqlCommandBuilder;
+        private MySqlDataAdapter mySqlDataAdapter;
+        private string query;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user id=Sergei;password=12345;persistsecurityinfo=True;database=lab4";
+
+            // костыль, чтобы после сохранения отобразить идшники
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            query = "SELECT * FROM Material";
+
+            mySqlDataAdapter = new MySqlDataAdapter(query, connection);
+            mySqlCommandBuilder = new MySqlCommandBuilder(mySqlDataAdapter);
+
+
+            dataTable = new DataTable();
+            mySqlDataAdapter.Fill(dataTable);
+
+            dataGridView1.DataSource = dataTable;
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.Columns["id"].ReadOnly = true;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            mySqlDataAdapter.Update(dataTable);
+
+            mySqlDataAdapter = new MySqlDataAdapter(query, connection);
+            mySqlCommandBuilder = new MySqlCommandBuilder(mySqlDataAdapter);
+
+            dataTable = new DataTable();
+            mySqlDataAdapter.Fill(dataTable);
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            dataTable.RejectChanges();
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            int selectedRow = dataGridView1.CurrentCell.RowIndex;
+            dataTable.Rows[selectedRow].Delete();
+            dataTable.AcceptChanges();
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["dateShipment"].Index)
+            {
+                dataGridView1.Rows[e.RowIndex].ErrorText = "";
+
+                if (dataGridView1.Rows[e.RowIndex].IsNewRow) { return; }
+                if (!(new Regex("[0-3][0-9].[0-1][0-9].20[0-2][0-9] [0-2][0-9]:[0-5][0-9]").Match(e.FormattedValue.ToString()).Success))
+                {
+                    e.Cancel = true;
+                    MessageBox.Show("Invalid date");
+                }
+            }
+        }
+    }
+}
